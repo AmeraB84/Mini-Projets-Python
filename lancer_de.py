53b37c17d0ed32ee6339da32ede4c7f254130fd6
Amera BOUDIA <amera.boudia84@gmail.com>
2021-11-03 16:45:01 +0100

    Lancer de dé

---
+++ lancer_de.py
@@ -0,0 +1,10 @@
+import random
+while True:
+    x = int(input('Please clic on any boutum and valid with enter '))
+    if x == 1:
+        print(random.randint(1, 6))
+    elif x == 0:
+        print('thank you , see you soon')
+        break
+    else:
+        print('bad input , please choose 1 or 0 to quit')
